a76188385e792aea522c27aaf76694edd7cb829f
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-15 13:53:28 +0300

    33

--- main.dart
+++ main.dart
@@ -1,3 +1,29 @@
+Stream<int> getNumbers() async* {
+  for (int i = 0; i < 5; i++) {
+    yield i;
+    await Future.delayed(Duration(seconds: 1));
+    // if (i == 2) {
+    //   throw Exception('An error occurred');
+    // }
+  }
+}
+
 void main(List<String> args) {
-  print('Hello world');
+ 
+
+  var subscription = getNumbers().listen((event) {
+    print(event);
+  });
+
+  subscription.onData((data) {
+    print('second data: $data');
+  });
+
+  subscription.onError((err) {
+    print(err);
+  });
+
+  subscription.onDone(() {
+    print('Stream is done.');
+  });
 }
